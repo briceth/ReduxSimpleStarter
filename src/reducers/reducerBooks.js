8d8540e0992f267961dbf9b3c47cba64a2557a1f
Brice Tessier Huort <tessierhuort@gmail.com>
2017-01-19 11:57:21 +0100

    create containers folder and put reducerbooks in it

---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: the good part'},
+    { title: 'Ruby on Rails'},
+    { title: 'Eloquent Ruby' },
+    { title: 'React on rails' }
+  ]
+}
